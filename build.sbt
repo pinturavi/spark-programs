884a8dd18df3286fe1df9f28503baa473decac12
pinturavi <pintu541796@gmail.com>
2019-07-16 21:08:40 +0530

    refactor code

--- build.sbt
+++ build.sbt
@@ -10,5 +10,8 @@ libraryDependencies += "com.typesafe.play" %% "play-json" % "2.7.1"
 libraryDependencies += "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.9.8"
 libraryDependencies += "com.crealytics" %% "spark-excel" % "0.11.1"
 libraryDependencies += "jasperreports" % "jasperreports" % "3.5.3"
+//libraryDependencies += "org.apache.spark" %% "spark-yarn" % "2.4.0"
+
+
 
 
--- src/main/scala/SampleApp.scala
+++ src/main/scala/SampleApp.scala
@@ -1,62 +1,73 @@
 import com.mongodb.spark.MongoSpark
-import org.apache.spark.sql.{Column, SparkSession, functions}
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.{Column, DataFrame, SparkSession, functions}
 
 object SampleApp {
 
   final val OUTPUT_FILE_PATH = "output.xlsx"
 
   def main(args: Array[String]): Unit = {
-    val spark = SparkSession.builder
-      .appName("SampleApp")
-      .master("local[4]")
-      .config("spark.mongodb.input.uri", "mongodb://127.0.0.1/test.myCollection")
-      .config("spark.mongodb.output.uri", "mongodb://127.0.0.1/test.myCollection")
-      .getOrCreate()
-    val ds = spark.read.format("csv")
-      .option("sep", ",")
-      .option("inferSchema", "true")
-      .option("header", "true")
-      .load("file:/home/ubuntu/spark_data/311data.csv")
-    val filteredDuplicateDS = ds.filter("DUPLICATE = true").groupBy(ds("SR_TYPE")).count//.select(ds("CITY"))
-    val filteredStattusDS = ds.groupBy(ds("STATUS")).count
-    val filteredCityDS = ds.groupBy(ds("CITY")).count
-    val filteredDS = ds.withColumn("CREATED", new Column(ds("CREATED_DATE").toString.split(" ")(0)))
-    val filteredCreatedDateDS = filteredDS.groupBy(filteredDS("STATUS"), filteredDS("CREATED")).count
-    filteredCreatedDateDS.show()
-   // MongoSpark.save(filteredDS.write.option("collection", "spark-1").mode("overwrite").format("com.mongodb.spark.sql"))
-    filteredDuplicateDS.write
-      .format("com.crealytics.spark.excel")
-      .option("dataAddress", "'Duplicate_Count[#All]")
-      .option("useHeader", "true")
-      .option("dateFormat", "yy-mmm-d") // Optional, default: yy-m-d h:mm
-      .option("timestampFormat", "mm-dd-yyyy hh:mm:ss") // Optional, default: yyyy-mm-dd hh:mm:ss.000
-      .mode("append") // Optional, default: overwrite.
-      .save(OUTPUT_FILE_PATH)
-    filteredStattusDS.write
-      .format("com.crealytics.spark.excel")
-      .option("dataAddress", "'Status_Count'!B3:C35")
-      .option("useHeader", "true")
-      .option("dateFormat", "yy-mmm-d") // Optional, default: yy-m-d h:mm
-      .option("timestampFormat", "mm-dd-yyyy hh:mm:ss") // Optional, default: yyyy-mm-dd hh:mm:ss.000
-      .mode("append") // Optional, default: overwrite.
-      .save(OUTPUT_FILE_PATH)
-    filteredCityDS.write
-      .format("com.crealytics.spark.excel")
-      .option("dataAddress", "'City_Count'!B3:C35")
-      .option("useHeader", "true")
-      .option("dateFormat", "yy-mmm-d") // Optional, default: yy-m-d h:mm
-      .option("timestampFormat", "mm-dd-yyyy hh:mm:ss") // Optional, default: yyyy-mm-dd hh:mm:ss.000
-      .mode("append") // Optional, default: overwrite.
-      .save(OUTPUT_FILE_PATH)
-    filteredCreatedDateDS.write
-      .format("com.crealytics.spark.excel")
-      .option("dataAddress", "'CreatedDate_Count'!B3:C35")
-      .option("useHeader", "true")
-      .option("dateFormat", "yy-mmm-d") // Optional, default: yy-m-d h:mm
-      .option("timestampFormat", "mm-dd-yyyy hh:mm:ss") // Optional, default: yyyy-mm-dd hh:mm:ss.000
-      .mode("append") // Optional, default: overwrite.
-      .save(OUTPUT_FILE_PATH)
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    val spark = getSparkSession
+    process(spark)
     spark.stop()
   }
 
+  private def process(spark: SparkSession): Unit = {
+    val df = readInput(spark).toDF()
+    df.registerTempTable("call_details")
+    df.show(3)
+    val data = spark.sql("SELECT SR_TYPE FROM call_details")
+    data.show
+    val filteredDuplicateDF = df.filter("DUPLICATE = true").groupBy(df("SR_TYPE")).count //.select(df("CITY"))
+    val filteredStatusDF = df.groupBy(df("STATUS")).count
+    val filteredCityDF = df.groupBy(df("CITY")).count
+    insertIntoMongo(filteredDuplicateDF, filteredStatusDF, filteredCityDF)
+    insertIntoExccel(filteredDuplicateDF, filteredStatusDF, filteredCityDF)
+  }
+
+  private def insertIntoMongo(filteredDuplicateDF: DataFrame, filteredStatusDF: DataFrame, filteredCityDF: DataFrame): Unit = {
+    writeToMongo(filteredDuplicateDF, "DUPLICATE_COUNT")
+    writeToMongo(filteredStatusDF, "STATUS_COUNT")
+    writeToMongo(filteredCityDF, "CITY_COUNT")
+  }
+
+  private def readInput(spark: SparkSession) = {
+    spark.read.format("csv")
+      .option("sep", ",")
+      .option("inferSchema", "true")
+      .option("header", "true")
+      //.load("file:/home/ubuntu/311data.csv")
+      .load("hdfs://localhost:9000/demo/311data.csv")
+  }
+
+  private def getSparkSession = {
+    SparkSession.builder
+      .appName("SampleApp")
+      .master("local[4]")
+      .config("spark.mongodb.input.uri", "mongodb://127.0.0.1/spark.myCollection")
+      .config("spark.mongodb.output.uri", "mongodb://127.0.0.1/spark.myCollection")
+      .getOrCreate()
+  }
+
+  private def insertIntoExccel(filteredDuplicateDF: DataFrame, filteredStatusDF: DataFrame, filteredCityDF: DataFrame): Unit = {
+    writeTpExcel(filteredDuplicateDF, "Duplicate_Count[#All]")
+    writeTpExcel(filteredStatusDF, "Status_Count[#All]")
+    writeTpExcel(filteredCityDF, "City_Count[#All]")
+  }
+
+  private def writeToMongo(df: DataFrame, collection: String): Unit = {
+    MongoSpark.save(df.write.option("collection", collection).mode("overwrite").format("com.mongodb.spark.sql"))
+  }
+
+  private def writeTpExcel(df: DataFrame, sheetName: String): Unit = {
+    df.write
+      .format("com.crealytics.spark.excel")
+      .option("dataAddress", sheetName)
+      .option("useHeader", "true")
+      .option("dateFormat", "yy-mmm-d") // Optional, default: yy-m-d h:mm
+      .option("timestampFormat", "mm-dd-yyyy hh:mm:ss") // Optional, default: yyyy-mm-dd hh:mm:ss.000
+      .mode("append") // Optional, default: overwrite.
+      .save(OUTPUT_FILE_PATH)
+  }
 }
